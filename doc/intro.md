df40354d0a542ca13fc077769a93c605ec689472
vlopatka <vadim.lopatka@gmail.com>
2023-04-22 06:21:43 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to all-time-top-countries
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/Vadym_Lopatka/all_time_top_countries_test.clj
@@ -0,0 +1,7 @@
+(ns Vadym-Lopatka.all-time-top-countries-test
+  (:require [clojure.test :refer :all]
+            [Vadym-Lopatka.all-time-top-countries :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

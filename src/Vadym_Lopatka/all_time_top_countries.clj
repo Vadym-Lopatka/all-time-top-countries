d7b0de115559432bb491590b2423947c3a9e3b90
vlopatka <vadim.lopatka@gmail.com>
2023-04-22 07:21:48 +0300

    added html page fetch

--- src/Vadym_Lopatka/all_time_top_countries.clj
+++ src/Vadym_Lopatka/all_time_top_countries.clj
@@ -1,12 +1,25 @@
 (ns Vadym-Lopatka.all-time-top-countries
-  (:gen-class))
+  (:gen-class)
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def what-i-need-to-do 
+  #{
+    "fetch the amount of existed data periods (consider drop down on the page)"
+    "fetch asked top by each period => should return coll of maps like {1 UK}" 
+    "map coll of maps to coll of sets" 
+    "find the all-time top via sets intersection"
+    })
+
+
+(def url "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2017")
+
+(defn get-html-content [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn find-the-tops []
+  (println (get-html-content url)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (find-the-tops))

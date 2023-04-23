c5addc4561dff5fde637473a8d0e9b237d34132d
vlopatka <vadim.lopatka@gmail.com>
2023-04-23 18:24:01 +0300

    almost all but still not work together

--- src/Vadym_Lopatka/all_time_top_countries.clj
+++ src/Vadym_Lopatka/all_time_top_countries.clj
@@ -1,6 +1,7 @@
 (ns Vadym-Lopatka.all-time-top-countries
   (:gen-class)
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.string :as cs]))
 
 (def what-i-need-to-do 
   #{
@@ -13,15 +14,65 @@
 
 (def numbeo-url "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp")
 
-(def numbeo-page-content (html/html-resource (java.net.URL. numbeo-url)))
+(defn page-content[url] 
+  (html/html-resource (java.net.URL. url)))
 
-(defn find-available-data-periods [] 
-  (let [drop-down-content (html/select numbeo-page-content [[:form.changePageForm html/first-of-type] :select :option])
-        periods (map html/text drop-down-content)]
+(defn find-available-data-periods []
+  (let [drop-down-content (html/select (page-content numbeo-url) [[:form.changePageForm html/first-of-type] :select :option])
+        periods (mapcat #(html/attr-values % :value) drop-down-content)]
     periods))
 
+;; (defn find-available-data-periods [] 
+;;   (let [drop-down-content (html/select (page-content numbeo-url) [[:form.changePageForm html/first-of-type] :select :option])
+;;         periods (map html/text drop-down-content)]
+;;     periods))
+
+(defn numbeo-period-url [period url]
+  (str url "?title=" period))
+
+(defn data-for-period [period]
+  (let [page (page-content (numbeo-period-url period numbeo-url))
+        countries (map html/text (html/select page [:table#t2 :tbody :tr]))]
+    countries))
+
+(defn to-countries-coll [raw-countries]
+  (let [texted-raw-strings (html/text raw-countries)
+        splitted (cs/split texted-raw-strings #"\n    ")
+        skipped-first-empty-string (rest splitted)
+        countries-data (map #(cs/replace % #"\n " "") skipped-first-empty-string)]
+
+    countries-data))
+
+(defn to-country-to-score-map [countries]
+  (into (sorted-map) 
+        (map (fn [country-coll] (assoc {}
+          (reduce + (map (fn [numberAsStr] (Double/parseDouble numberAsStr)) (rest country-coll)))
+          (first country-coll))) countries)))
+
+(defn take-top [amount ordered-countries]
+  (take-last amount ordered-countries))
+
+(defn get-for-period [period]
+  (let [data (data-for-period period)
+        data-as-coll (map to-countries-coll data)
+        scores (to-country-to-score-map data-as-coll)
+        top (take-top 10 scores)]
+    
+    top))
+
+(doseq )
+
+;; (to-country-to-score-map (map to-countries-coll n))
+
+(defn find-top-of-all-time []
+  (let [periods (find-available-data-periods)
+        period-to-countries (map (fn [period] (assoc {} period (take-top 10 (to-country-to-score-map (to-countries-coll (data-for-period period)))))) periods)
+        ]
+    period-to-countries))
+
+(def periods find-available-data-periods)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (find-available-data-periods)))
+  (println (find-top-of-all-time 5)))
--- src/Vadym_Lopatka/data.clj
+++ src/Vadym_Lopatka/data.clj
@@ -1,29 +1,5 @@
-;; (ns Vadym-Lopatka.data)
+(ns Vadym-Lopatka.data)
 
-;; (2023 2022 Mid-Year 2022 2021 Mid-Year 2021 2020 Mid-Year 2020 2019 Mid-Year 2019 2018 Mid-Year 2018 2017 Mid-Year 2017 2016 Mid-Year 2016 2015 Mid-Year 2015 2014 Mid-Year 2014 2013 2012)
-
-;; (
-;;  #{:tag :option, :attrs {:value 2023, :selected selected}, :content (2023)} 
-;;  #{:tag :option, :attrs {:value 2022-mid}, :content (2022 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2022}, :content (2022)} 
-;;  #{:tag :option, :attrs {:value 2021-mid}, :content (2021 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2021}, :content (2021)} 
-;;  #{:tag :option, :attrs {:value 2020-mid}, :content (2020 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2020}, :content (2020)} 
-;;  #{:tag :option, :attrs {:value 2019-mid}, :content (2019 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2019}, :content (2019)} 
-;;  #{:tag :option, :attrs {:value 2018-mid}, :content (2018 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2018}, :content (2018)} 
-;;  #{:tag :option, :attrs {:value 2017-mid}, :content (2017 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2017}, :content (2017)} 
-;;  #{:tag :option, :attrs {:value 2016-mid}, :content (2016 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2016}, :content (2016)} 
-;;  #{:tag :option, :attrs {:value 2015-mid}, :content (2015 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2015}, :content (2015)} 
-;;  #{:tag :option, :attrs {:value 2014-mid}, :content (2014 Mid-Year)} 
-;;  #{:tag :option, :attrs {:value 2014}, :content (2014)} 
-;;  #{:tag :option, :attrs {:value 2013-Q1}, :content (2013)} 
-;;  #{:tag :option, :attrs {:value 2012-Q1}, :content (2012)})
-
-
-
+(def v (list
+        `("Netherlands" "196.7" "107.8" "72.7" "77.1" "68.6" "7.2" "25.5" "23.7" "86.8") 
+  `("Denmark" "194.7" "105.0" "73.4" "79.2" "78.6" "6.6" "28.5" "21.3" "81.8")))

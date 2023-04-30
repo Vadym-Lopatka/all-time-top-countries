a07963d2df43e6326505aa4a965487f6396bac8e
vlopatka <vadim.lopatka@gmail.com>
2023-04-30 21:54:49 +0300

    cleanup periods

--- src/Vadym_Lopatka/all_time_top_countries.clj
+++ src/Vadym_Lopatka/all_time_top_countries.clj
@@ -8,12 +8,10 @@
   (let [periods (period/fetch-available-data-periods)] 
     (countries/get-countries-for-periods periods)))
 
-
 (defn find-all-time-top-countries [size-of-top]
   (let [data (map first (map vals (get-all-periods-data)))
         tops (map #(take-last size-of-top %) data)
-        country-names (map vals tops)
-        ]
+        country-names (map vals tops)]
     (reduce set/intersection  (map set country-names))))
 
 (defn -main
--- src/Vadym_Lopatka/numbeo/periods.clj
+++ src/Vadym_Lopatka/numbeo/periods.clj
@@ -2,13 +2,14 @@
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as cs]))
 
+(def url "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp")
 
-(def numbeo-url "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp")
+(def dropdown-periods-selector [[:form.changePageForm html/first-of-type] :select :option])
 
 (defn page-content [url]
   (html/html-resource (java.net.URL. url)))
 
 (defn fetch-available-data-periods []
-  (let [drop-down-content (html/select (page-content numbeo-url) [[:form.changePageForm html/first-of-type] :select :option])
+  (let [drop-down-content (html/select (page-content url) dropdown-periods-selector)
         periods (mapcat #(html/attr-values % :value) drop-down-content)]
     periods))
